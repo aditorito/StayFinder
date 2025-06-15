9ea1e66c5aaa1d996ad5ec562768c400af38f752
Aditya <as0742004@gmail.com>
2025-06-16 00:11:03 +0530

    Listing model created

--- server/models/Listing.js
+++ server/models/Listing.js
@@ -1,5 +1,51 @@
 const mongoose = require('mongoose');
 
-const listingSchema = mongoose.Schema({
-    
+const locationSchema = new mongoose.Schema({
+    type:{
+        type:String,
+        enum:['Point'],
+        default:'Point'
+    },
+    coordinates:{
+        type:[Number],
+        required: true
+    },
+    address: {
+        type:String,
+        required:true
+    }
 })
+const listingSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    location:locationSchema,
+    pricePerNight:{
+        type:Number,
+        required:true
+    },
+    images:{
+        type:[String],
+        required:true
+    },
+    hostId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    availability:{
+        from:Date,
+        to:Date
+    }
+});
+
+const Listings = mongoose.model('Listings', listingSchema);
+
+module.exports = Listings;
+
