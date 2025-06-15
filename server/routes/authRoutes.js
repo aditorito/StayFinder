d2c4e32fc10bf412f1eb8db820bd0bab618c941f
Aditya <as0742004@gmail.com>
2025-06-15 20:45:27 +0530

    signin routes added

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRouter = express.Router();
 const authController = require('../controllers/authControllers')
 
-authRouter.post('/signup', authController.signup)
+authRouter.post('/signup', authController.signup);
+authRouter.post('/signin', authController.signin);
 
 module.exports = authRouter;
