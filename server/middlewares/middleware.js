c379c751b25d9a7bcde24290dbd144e6a160d12b
Aditya <as0742004@gmail.com>
2025-06-16 00:22:44 +0530

    auth middleware is added

---
+++ server/middlewares/middleware.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        if (!token) {
+            return res.status(403).json({
+                message: "User doesn't have permission",
+                isauthorized: false
+
+            })
+        }
+        const decoded = jwt.decode(token, JWT_SECRET);
+        req.usedId = decoded.usedId;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            message: "User is not logged In",
+            isauthorized: false
+
+        })
+
+    }
+}
+
+exports
