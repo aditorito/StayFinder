2b017e564eade9f58c0e05a6abcfab6180cf840a
Aditya <as0742004@gmail.com>
2025-06-16 14:15:08 +0530

    added user route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRouter = express.Router();
-const authController = require('../controllers/authControllers')
+const authController = require('../controllers/authControllers');
+const authMiddleware = require('../middlewares/middleware');
 
 authRouter.post('/register', authController.register);
 authRouter.post('/login', authController.login);
+authRouter.get('/user', authMiddleware,authController.getUserdetails);
 
 module.exports = authRouter;
