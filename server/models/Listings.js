a5562d1bf2c5d6c1aa641b74f8e2ed788d64e629
Aditya <as0742004@gmail.com>
2025-06-16 15:15:08 +0530

    updated listing model

---
+++ server/models/Listings.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const locationSchema = new mongoose.Schema({
+    type:{
+        type:String,
+        enum:['Point'],
+        default:'Point'
+    },
+    coordinates:{
+        type:[Number],
+        required: true
+    },
+    address: {
+        type:String,
+        required:true
+    }
+})
+
+const listingSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    location:locationSchema,
+    pricePerNight:{
+        type:Number,
+        required:true
+    },
+    images:{
+        type:[String],
+        required:true
+    },
+    hostId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Users',
+        required:true
+    },
+    availability:{
+        from:Date,
+        to:Date
+    }
+});
+
+const Listings = mongoose.model('Listings', listingSchema);
+
+module.exports = Listings;
+
