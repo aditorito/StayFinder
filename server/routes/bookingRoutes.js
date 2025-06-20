13b2d4f010ed2252acf99d1d681bc52226c0819c
Aditya <as0742004@gmail.com>
2025-06-21 02:37:40 +0530

    upload route for images

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -3,6 +3,10 @@ const bookingRouter = express.Router();
 const authMiddleware = require('../middlewares/middleware');
 const bookingController = require('../controllers/bookingControllers');
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 bookingRouter.post('/', authMiddleware, bookingController.booking);
+bookingRouter.post('/upload', authMiddleware, upload.single('image'), bookingController.upload);
 
 module.exports = bookingRouter;
