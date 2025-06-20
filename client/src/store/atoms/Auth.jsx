44595f5beb21b62fbc17acbc9c3d8443e26a4250
Aditya <as0742004@gmail.com>
2025-06-20 23:48:56 +0530

    added recoil state for auth

---
+++ client/src/store/atoms/Auth.jsx
@@ -0,0 +1,26 @@
+import { atom } from "recoil";
+
+export const isLoggedInAtom = atom({
+  key: "isLoggedInAtom",
+  default: false, // false until login succeeds
+});
+
+export const nameAtom = atom({
+    key:"nameAtom",
+    default:''
+})
+
+export const roleAtom = atom({
+    key:'roleAtom',
+    default:'guest'
+})
+
+export const emailAtom = atom({
+    key:"emailAtom",
+    default:''
+});
+
+export const passwordAtom = atom({
+    key:"passwodAtom",
+    default:''
+})
